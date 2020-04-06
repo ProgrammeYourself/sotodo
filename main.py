db776b8668a3ce1ca46562812852e50e6f1727a8
Riedler <riedler@gelse.eu>
2020-04-06 15:19:57 +0200

    fixed weird JSONDecodeError and various other details

--- main.py
+++ main.py
@@ -2,54 +2,54 @@
 import sys, json
 
 class Application:
-    def __init__(self, workload, window=None):
-        self.workload = workload.data
-        self.window = window if window != None else None
+	def __init__(self, workload, window=None):
+		self.workload = workload.data
+		self.window = window if window != None else None
 
 class Workload:
-    def __init__(self, file):
-        with open(file, "r") as fs:
-            self.data = json.load(fs)
+	def __init__(self, file):
+		with open(file, "r") as fs:
+		    self.data = json.load(fs)
 
-    def get_fixed(self):
-        if "fixed" in self.data:
-            return self.data["fixed"]
-        else:
-            return ValueError("Error: subject has no fixed field")
+	def get_fixed(self):
+		if "fixed" in self.data:
+		    return self.data["fixed"]
+		else:
+		    return ValueError("Error: subject has no fixed field")
 
-    def get_modules(self):
-        return self.data["modules"]
+	def get_modules(self):
+		return self.data["modules"]
 
 class Window:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
 class ArgumentHandler:
-    def __init__(self, argv):
-        self.argv = argv
-        self.subjects = json.load(open("data/workloads.json", "r"))["workloads"]
+	def __init__(self, argv):
+		self.argv = argv
+		self.subjects = json.load(open("./data/workloads.json", "r"))["workloads"]
 
-    def check(self):
-        if len(self.argv) == 1: return
-        if self.argv[1] == "-m":
-            if self.argv[4] not in self.subjects: return
-            fs = open("data/{}.json".format(self.argv[4]), "r")
-            data = json.load(fs)
-            data["modules"].append({"name": "{}".format(self.argv[2]), "description": "{}".format(self.argv[3])})
-            fs = open("data/{}.json".format(self.argv[4]), "w")
-            json.dump(data, fs, indent=4)
-
-        
-        # elif self.argv[1] == "-f":
-        #     if self.argv[4] not in self.subjects: return
-        #     fs = open("data/{}.json".format(self.argv[4]), "r")
-        #     if "fixed" not in fs.read(): return
-
-        #     data = json.load(fs)           ==> throws decoding error
-        
-        #     data["fixed"]["{}".format(self.argv[2])] = "{}".format(self.argv[3])
-        #     fs = open("data/{}.json".format(self.argv[4]), "w")
-        #     json.dump(data, fs, indent=4)
+	def check(self):
+		if len(self.argv) == 1:
+			return
+		if self.argv[1] == "-m":
+			if self.argv[4] not in self.subjects:
+				return
+			with open("data/%s.json"%self.argv[4], "r") as fs:
+				data = json.load(fs)
+			data["modules"].append({"name": self.argv[2], "description": self.argv[3]})
+			with open("data/%s.json"%self.argv[4], "w") as fs:
+				json.dump(data, fs, indent="\t")
+		elif self.argv[1] == "-f":
+			if self.argv[4] not in self.subjects:
+				return
+			with open("data/%s.json"%self.argv[4], "r") as fs:
+				data = json.load(fs)
+			if "fixed" not in data.keys():
+				data["fixed"]={}
+			data["fixed"][self.argv[2]] = self.argv[3]
+			with open("data/%s.json"%self.argv[4], "w") as fs:
+				json.dump(data, fs, indent="\t")
 
 
 h = ArgumentHandler(sys.argv)
