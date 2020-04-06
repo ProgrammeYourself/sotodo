7352d8089af2d4de8397a6cfa12fe5eeca6d10ec
ProgrammeYourself <andi.suender@outlook.de>
2020-04-06 12:11:45 +0200

    created classes

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Application:
+    def __init__(self):
+        pass
+
+class Workload:
+    def __init__(self):
+        pass
+
+class Window:
+    def __init__(self):
+        pass
