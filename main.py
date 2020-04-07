53bf5c15033071f9d969f4a43e8bfc0781b7775a
ProgrammeYourself <andi.suender@outlook.de>
2020-04-07 10:15:07 +0200

    ArgumentHandler is now a function

--- main.py
+++ main.py
@@ -27,49 +27,44 @@ class Window:
 	def __init__(self):
 		pass
 
-class ArgumentHandler:
-	def __init__(self, argv):
-		self.argv = argv
-		self.subjects = json.load(open("./data/workloads.json", "r"))["workloads"]
+def ArgumentHandler(argv):
+    subjects = json.load(open("./data/workloads.json", "r"))["workloads"]
+    argc=len(argv)
+    if argc == 1:
+        return
+    if argv[1] == "-m":
+        if argc<6:
+            raise CommonCode(3,"-m",",".join(["number","name","description","category"][len(self.argv)-2:]))
+        elif argc>6:
+            raise CommonCode(4,"-m",str(argc-6))
+        elif argv[5] not in subjects:
+            raise CommonCode(7,"-m",argv[5],": category does not exist")
+        with open("data/%s.json"%argv[5], "r") as fs:
+            data = json.load(fs)
+        if "modules" not in data.keys():
+            data["modules"]={argv[2]:{"name":argv[3],"desc":argv[4]}}
+        else:
+            data["modules"][argv[2]] = {"name":argv[3],"desc":argv[4]}
+        with open("data/%s.json"%argv[5], "w") as fs:
+            json.dump(data, fs, indent="\t")
+    elif argv[1] == "-f":
+        if argc<5:
+            raise CommonCode(3,"-m",",".join(["name","description","category"][len(self.argv)-2:]))
+        elif argc>5:
+            raise CommonCode(4,"-m",str(argc-6))
+        elif argv[4] not in subjects:
+            raise CommonCode(7,"-f",argv[4],": category does not exist")
+        with open("data/%s.json"%argv[4], "r") as fs:
+            data = json.load(fs)
+        if "fixed" not in data.keys():
+            data["fixed"]={argv[2]:argv[3]}
+        else:
+            data["fixed"][argv[2]] = argv[3]
+        with open("data/%s.json"%argv[4], "w") as fs:
+            json.dump(data, fs, indent="\t")
+    else:
+        raise CommonCode(6,argv[1])
 
-	def check(self):
-		argc=len(self.argv)
-		if argc == 1:
-			return
-		if self.argv[1] == "-m":
-			if argc<6:
-				raise CommonCode(3,"-m",",".join(["number","name","description","category"][len(self.argv)-2:]))
-			elif argc>6:
-				raise CommonCode(4,"-m",str(argc-6))
-			elif self.argv[5] not in self.subjects:
-				raise CommonCode(7,"-m",self.argv[5],": category does not exist")
-			with open("data/%s.json"%self.argv[5], "r") as fs:
-				data = json.load(fs)
-			if "modules" not in data.keys():
-				data["modules"]={self.argv[2]:{"name":self.argv[3],"desc":self.argv[4]}}
-			else:
-				data["modules"][self.argv[2]] = {"name":self.argv[3],"desc":self.argv[4]}
-			with open("data/%s.json"%self.argv[5], "w") as fs:
-				json.dump(data, fs, indent="\t")
-		elif self.argv[1] == "-f":
-			if argc<5:
-				raise CommonCode(3,"-m",",".join(["name","description","category"][len(self.argv)-2:]))
-			elif argc>5:
-				raise CommonCode(4,"-m",str(argc-6))
-			elif self.argv[4] not in self.subjects:
-				raise CommonCode(7,"-f",self.argv[4],": category does not exist")
-			with open("data/%s.json"%self.argv[4], "r") as fs:
-				data = json.load(fs)
-			if "fixed" not in data.keys():
-				data["fixed"]={self.argv[2]:self.argv[3]}
-			else:
-				data["fixed"][self.argv[2]] = self.argv[3]
-			with open("data/%s.json"%self.argv[4], "w") as fs:
-				json.dump(data, fs, indent="\t")
-		else:
-			raise CommonCode(6,self.argv[1])
-
-h = ArgumentHandler(sys.argv)
-h.check()
+ArgumentHandler(sys.argv)
 # w = Workload("data/medt.json")
 # a = Application(w, "Hello")
